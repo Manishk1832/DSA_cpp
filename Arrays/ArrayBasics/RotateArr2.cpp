3079a6c5fc4a2940afe318ea17e3fdb9c2e8ba26
manish kumar <mk1169852@gmail.com>
2023-01-25 21:31:09 +0530

    modified:   Arrays/ArrayBasics/RotateArr2.cpp 	new file:   Arrays/ArrayBasics/RotateArr2.exe

--- Arrays/ArrayBasics/RotateArr2.cpp
+++ Arrays/ArrayBasics/RotateArr2.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 void Rev(int arr[], int s, int e, int n)
 {
-    int s = 0;
-    int e = n - 1;
+
     for (int i = 0; i < n; i++)
     {
         if (s < e)
@@ -21,13 +20,20 @@ void Rev(int arr[], int s, int e, int n)
 void Rotate(int arr[], int rot, int n)
 {
 
-    Rev(arr,  0, n - 1, n);
+    rot = rot % n;
+    Rev(arr, 0, n - 1, n);
+    Rev(arr, 0, rot - 1, n);
+    Rev(arr, rot, n - 1, n);
 }
 
 int main()
 {
     int n = 5;
     int arr[] = {10, 20, 30, 40, 50};
-    int rot = 3;
-    Rotate(arr, n, rot);
+    int rot = 0;
+    Rotate(arr, rot, n);
+    for (int print : arr)
+    {
+        cout << print << " ";
+    }
 }
